121bfc4cf73a9a2d115fe8a2273707ae54c248bf
M-Salloum-M <mohammadsalloum12345@outlook.com>
2021-08-30 14:05:24 +0300

    enabling redis cache

--- src/main/java/com/example/CodeChallenge003MohammadSalloum/CodeChallenge003MohammadSalloumApplication.java
+++ src/main/java/com/example/CodeChallenge003MohammadSalloum/CodeChallenge003MohammadSalloumApplication.java
@@ -2,8 +2,10 @@ package com.example.CodeChallenge003MohammadSalloum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CodeChallenge003MohammadSalloumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/CodeChallenge003MohammadSalloum/controller/CustomerController.java
+++ src/main/java/com/example/CodeChallenge003MohammadSalloum/controller/CustomerController.java
@@ -4,6 +4,9 @@ import com.example.CodeChallenge003MohammadSalloum.model.Customer;
 import com.example.CodeChallenge003MohammadSalloum.service.CustomerService;
 import com.example.CodeChallenge003MohammadSalloum.service.RabbitMQSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,19 +26,19 @@ public class CustomerController {
     @Autowired
     RabbitMQSender rabbitMQSender;
 
-
+    @Cacheable(value = "customers", key = "#id")
     @GetMapping("/getAll")
     List<Customer> getAll() {
         List<Customer> customers = customerService.findAll();
         return customers;
     }
-
+    @Cacheable(value = "customers", key = "#id")
     @GetMapping("/{id}")
     Optional<Customer> get(@PathVariable UUID id) {
         Optional<Customer> customer = customerService.findById(id);
         return customer;
     }
-
+    @CachePut(value = "customers", key = "#id")
     @PostMapping
     Customer newOne(@RequestBody Customer t) {
         ResponseEntity<?> responseEntity = validateNumberController.validate(t.getMobileNumber());
@@ -46,7 +49,7 @@ public class CustomerController {
             return customerService.save(t);
         }
     }
-
+    @CachePut(value = "customers", key = "#customers.id")
     @PutMapping
     Customer update(@RequestBody Customer t) {
         ResponseEntity<?> responseEntity = validateNumberController.validate(t.getMobileNumber());
@@ -55,7 +58,7 @@ public class CustomerController {
         } else
             return customerService.save(t);
     }
-
+    @CacheEvict(value = "customers", allEntries=true)
     @DeleteMapping("/{id}")
     void delete(@PathVariable UUID id) {
         customerService.deleteById(id);
